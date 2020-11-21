7dba98a80b7d6bf62927f9be1b205a3c4545d43c
Andrew Goncharov <andrew208132@gmail.com>
2020-11-21 12:41:18 +0300

    Third test

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-import telebot;
+import telebot
 bot = telebot.TeleBot('%1465761077:AAG3N5kLxtGAKmxNLzT16_u7AXRWn3mnhMo%')
 
 
@@ -6,7 +6,7 @@ bot = telebot.TeleBot('%1465761077:AAG3N5kLxtGAKmxNLzT16_u7AXRWn3mnhMo%')
 def welcome_start(message):
 	bot.send_message(message.chat.id, 'Добрый день, User.')
 
-def welcome_start(message):
+def welcome_help(message):
 	bot.send_message(message.chat.id, 'Как тебе помочь?')
 	
 
@@ -17,16 +17,16 @@ age = 0
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
 	if message.text == "Начать очередь":
-    		bot.send_message(message.from_user.id, "Создание списка")
+		bot.send_message(message.from_user.id, "Создание списка")
 	else:
-   		bot.send_message(message.from_user.id, "Напиши /help.")
+		bot.send_message(message.from_user.id, "Напиши /help.")
 
 def start(message):
 	if message.text == '/reg':
-        	bot.send_message(message.from_user.id, "Как тебя зовут?")
-        	bot.register_next_step_handler(message, get_name) #следующий шаг – функция get_name
-    	else:
-        	bot.send_message(message.from_user.id, 'Напиши /reg')
+		bot.send_message(message.from_user.id, "Как тебя зовут?")
+		bot.register_next_step_handler(message, get_name) #следующий шаг – функция get_name
+	else:
+		bot.send_message(message.from_user.id, 'Напиши /reg')
 
 def get_name(message):
 	global name
