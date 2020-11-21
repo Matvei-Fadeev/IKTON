25927c8f26e8ce85fe895aefc451ef983d07f8dd
Matvei-Fadeev <matwei.fadeew@gmail.com>
2020-11-21 20:25:51 +0300

    Change client

--- interface/send_data.py
+++ interface/send_data.py
@@ -1,11 +1,15 @@
 import socket
 
-HOST = '127.0.0.1'
-PORT = 2222
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s: 
-    s.connect((HOST, PORT))
-    s.sendall(b'Hello, world')
-    data = s.recv(1024)
+
+def send_data(send_data, HOST='127.0.0.1', PORT=2222):
+    received_data = b''
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((HOST, PORT))
+        s.sendall(send_data)
+        received_data = s.recv(1024)
+    return received_data
 
 if "__main__" == __name__:
-    pass
+    print(send_data(b"create|qwe|qwe|123|qwe"))
+    print(send_data(b"afsas"))
+    print(send_data(b"12312"))
