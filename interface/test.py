318adf4f9e71dc5ecf215e0062dce3a40d729a0d
Matvei-Fadeev <matwei.fadeew@gmail.com>
2020-11-21 14:27:18 +0300

    Fix code style

--- interface/test.py
+++ interface/test.py
@@ -14,18 +14,22 @@ name = ''
 surname = ''
 age = 0
 
+def get_surname(message):
+	bot.send_message(message.from_user.id, 'Назови свою фамилию.')
+	global surname
+	surname = message.text
+
 def get_name(message):
+	bot.send_message(message.from_user.id, "Как тебя зовут?")
 	global name
 	name = message.text
-	bot.send_message(message.from_user.id, 'Назови свою фамилию.')
-	bot.register_next_step_handler(message, getsurname)
+
 
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
 	if message.text == "Начать очередь":
 		bot.send_message(message.from_user.id, "Создание списка")
 	elif message.text == '/reg':
-		bot.send_message(message.from_user.id, "Как тебя зовут?")
 		bot.register_next_step_handler(message, get_name) #следующий шаг – функция get_name
 	else:
 		bot.send_message(message.from_user.id, "Напиши /help.")
@@ -34,11 +38,8 @@ def start(message):
 	if message.text == '/reg':
 		bot.send_message(message.from_user.id, "Как тебя зовут?")
 		bot.register_next_step_handler(message, get_name) #следующий шаг – функция get_name
+		bot.register_next_step_handler(message, getsurname)
 	else:
 		bot.send_message(message.from_user.id, 'Напиши /reg')
-
-def get_surname(message):
-	global surname
-	surname = message.text
 
 bot.polling(none_stop=True, interval=0)
