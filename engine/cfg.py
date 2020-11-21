c6fe2bfc878c11c3d5a02ea24c5317cdb5eb3550
Matvei-Fadeev <matwei.fadeew@gmail.com>
2020-11-21 20:22:59 +0300

    Serverside processing + client side testing

--- engine/cfg.py
+++ engine/cfg.py
@@ -1,23 +1,23 @@
 # data to configurate server to working
-separateSymbol = '|'
+separateSymbol = "|"
 HOST = '0.0.0.0'
 PORT = 2222
+
+db_files_location = "./db_queues/"
 g_count_of_received_symbols = 1000
 # g_user_db_path = "user_db.sql"
-g_empty_response = '0'
+g_empty_response = ''
 g_max_attempts = 10
 g_error_sleep_sec = 0.04
 
 
+"CMD|KIGK3122|MySuperNickname|TelegramID|"
+CMD = 0
+LABEL = 1
+NAME = 2
+tID = 3
+DATA_COUNT = 4
 
-class db_rows(object):
-    ID = 0
-    LICENSE_KEY = 1
-    HWID = 2
-    VALIDITY_DAYS = 3
-    EXPIRATION_DATE = 4
-    SUBSCRIBE_TYPE = 5
-    ROWS_COUNT = 6
 
 g_db_full_row = "*"
 g_db_table_name = "users_licenses"
--- engine/process_data.py
+++ engine/process_data.py
@@ -1,5 +1,67 @@
+import cfg
+
+"""
+    import
+
+"""
+
+def add_user(label, name, telegram_id):
+    pass
+
+def remove_user(label, name, telegram_id):
+    pass
+
+
+def db_exist(path) -> bool:
+    """
+        check for existing of file in Unix system
+        find function
+    """
+
+def create_table(label):
+    # current folder is IKTON/engine
+    # path "./db_queues/kigk3122.sql"
+    path = cfg.db_files_location + label + ".sql"
+    if db_exist(path):
+        return false
+    else:
+        """ Creating the new db
+            Lib for working with Linux
+            Function to copy file using template.sql
+        """
+
+def get_table(label):
+    """
+        Check existing of file by label
+            Read table
+        Else
+    """
 
 
 def process_user_data(data):
-    print("Hi")
-    pass
+    if !data:
+        return cfg.g_empty_response
+
+    """
+        Parse data
+        #user_data = data.split(cfg.separateSymbol)
+    """
+    response = [] # !! hz
+
+    cmd = user_data[cfg.CMD]
+    if cmd == "create":
+        create_table(label)
+    elif cmd == "show":
+        response = get_table(label)
+    elif len(user_data) >= cfg.DATA_COUNT:
+        label = user_data[cfg.LABEL].lower()
+        name = user_data[cfg.NAME].lower()
+        telegram_id = user_data[cfg.TID]
+
+        if cmd == "add":
+            add_user(label, name, telegram_id)
+        elif cmd == "remove":
+            remove_user(label, name, telegram_id)
+
+
+    print(data)
